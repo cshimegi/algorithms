ea2edd067ff04e3c82f6981b0ca3b6213738610f
Alan(ChenHao,Chan) <cs-himegi@hotmail.com>
2024-12-02 23:28:29 +0900

    Leetcode 1455

---
+++ leetcode/easy/Problem1455.java
@@ -0,0 +1,27 @@
+package leetcode.easy;
+
+
+/**
+ * Leetcode 1455
+ * <p>
+ * Link: <a href="https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/description/">Problem</a>
+ * <p>
+ * Hint:
+ * */
+public class Problem1455 {
+    public static int isPrefixOfWord(String sentence, String searchWord) {
+        String[] words = sentence.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].startsWith(searchWord)) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrefixOfWord("i love eating burger", "burg"));
+        System.out.println(isPrefixOfWord("this problem is an easy problem", "pro"));
+        System.out.println(isPrefixOfWord("i am tired", "you"));
+    }
+}
