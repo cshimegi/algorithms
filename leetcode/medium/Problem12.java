82147d3d0768b7d1a61c003c3e1045aec25a8cbd
Alan(ChenHao,Chan) <cs-himegi@hotmail.com>
2024-12-09 23:53:18 +0900

    Leetcode 12

---
+++ leetcode/medium/Problem12.java
@@ -0,0 +1,34 @@
+package leetcode.medium;
+
+
+import java.util.Arrays;
+
+/**
+ * Leetcode 12
+ * <p>
+ * Link: <a href="https://leetcode.com/problems/integer-to-roman/description/">Problem</a>
+ * <p>
+ * Hint:
+ * */
+public class Problem12 {
+    public static String intToRoman(int num) {
+        final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        final String[] symbols = {"M", "CM", "D",  "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < values.length; ++i) {
+            if (num == 0) break;
+            while (num >= values[i]) {
+                sb.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(intToRoman(3));
+        System.out.println(intToRoman(58));
+        System.out.println(intToRoman(1994));
+    }
+}
