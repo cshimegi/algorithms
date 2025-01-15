6a55c567f0dd4ee9f11a85cf06a5d8dc73c79300
Alan(ChenHao,Chan) <cs-himegi@hotmail.com>
2025-01-15 23:45:19 +0900

    Leetcode 8,2429 python3

---
+++ leetcode/python/medium/Problem2429.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        # Count the number of 1's in num1 and num2
+        num1_count1, num2_count1 = num1.bit_count(), num2.bit_count()
+
+        # Assume x since x XOR num1 is minimal
+        x = num1
+
+        # Assuem machine is using 32 bit
+        for i in range(32):
+            # Remove the position of 1 in x because num1_count1 > num2_count1 and
+            # it needs to be the same bit sets as num2 by decreasing the number of 1's
+            if num1_count1 > num2_count1 and (1 << i) & num1 > 0:
+                x ^= 1 << i
+                num1_count1 -= 1
+            # Other the other hand, it needs to add more bit sets to be the same bit set as num1
+            # by increasing the number of 1's because num1_count1 < num2_count1
+            if num1_count1 < num2_count1 and (1 << i) & num1 == 0:
+                x ^= 1 << i
+                num1_count1 += 1
+
+        return x
+
+# Problem 2429
+# Link: https://leetcode.com/problems/minimize-xor/description/
+if __name__ == '__main__':
+    s = Solution()
+    print(s.minimizeXor(3, 5))  # 3
+    print(s.minimizeXor(1, 12))  # 3
---
+++ leetcode/python/medium/Problem8.py
@@ -0,0 +1,51 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        MIN_INT, MAX_INT = -2**31, 2**31-1
+
+        ans = 0
+        first_char = None
+        is_negative = False
+        for c in s:
+            # Ignore leading spaces
+            if first_char is None and c == " ":
+                continue
+
+            if first_char is None:
+                first_char = c
+                if first_char == "-" or first_char == "+":
+                    is_negative = first_char == "-"
+                elif first_char.isdigit():
+                    ans = ans * 10 + int(first_char)
+                else:
+                    break
+            else:
+                if c.isdigit():
+                    ans = ans * 10 + int(c)
+                else:
+                    break
+
+        if is_negative:
+            ans *= -1
+            if ans < MIN_INT:
+                ans = MIN_INT
+        else:
+            if ans > MAX_INT:
+                ans = MAX_INT
+
+        return ans
+
+
+# Problem 8
+# Link: https://leetcode.com/problems/string-to-integer-atoi//description/
+if __name__ == '__main__':
+    s = Solution()
+    print(s.myAtoi("4.2"))
+    print(s.myAtoi("   -042"))
+    print(s.myAtoi("words and 987"))
+    print(s.myAtoi("42"))
+    print(s.myAtoi("1337c0d3"))
+    print(s.myAtoi("0-1"))
+    print(s.myAtoi("2147483648"))
+    print(s.myAtoi("-2147483649"))
+    print(s.myAtoi("31474836488"))
+    print(s.myAtoi("   +0 123"))
